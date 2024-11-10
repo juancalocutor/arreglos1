36dc5a031cfc614f9c69c8897f2ccb161eb378e8
juancalocutor <juancalocutor@gmail.com>
2024-11-10 17:27:42 -0500

    iniciamos con arreglos iterar imprimir

--- src/arreglos_2_ordenar_sort.java
+++ src/arreglos_2_ordenar_sort.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class arreglos_2_ordenar_sort {
     public static void main(String[] args) {
         String[] productos = new String[7];
+        int total = productos.length;
 
         productos[0] = "CocaCola con Pan";
         productos[1] = "CocaCola con Roscón";
@@ -12,7 +13,7 @@ public class arreglos_2_ordenar_sort {
         productos[5] = "CocaCola con Papa";
         productos[6] = "CocaCola con Hojaldra";
 
-        System.out.println("\nSe imprime sin ordenar");
+        System.out.println("\nSe imprime sin ordenar uno por uno");
 
         System.out.println(productos[0]);
         System.out.println(productos[1]);
@@ -21,6 +22,12 @@ public class arreglos_2_ordenar_sort {
         System.out.println(productos[4]);
         System.out.println(productos[5]);
         System.out.println(productos[6]);
+
+        System.out.println("\nSe imprime sin ordenar con ciclo for");
+
+        for (int i = 0; i < total; i++) {
+            System.out.println("Productos[" + (i + 1) +"] = " + productos[i]);
+        }
 
         Arrays.sort(productos);
 
@@ -33,6 +40,30 @@ public class arreglos_2_ordenar_sort {
         System.out.println(productos[4]);
         System.out.println(productos[5]);
         System.out.println(productos[6]);
+
+        System.out.println("\nSe imprime ordenado con ciclo foreach");
+
+        for (String string : productos) {
+            System.out.println("Productos = " + string);
+        }
+
+        System.out.println("\nSe imprime con ciclo while");
+
+        int i = 0;
+
+        while (i < total) {
+            System.out.println("Producto " + (i+1) + " = " + productos[i]);
+            i++;
+        }
+
+        System.out.println("\nSe imprime con el ciclo do while");
+
+        int j = 0;
+
+        do {
+            System.out.println("Producto " + (j+1) + " = " + productos[j]);
+            j++;
+        } while (j < total);
       
     }
 }
